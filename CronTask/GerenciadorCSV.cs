01108a1a471043f03dd89bd21a67db359862b67e
GabrielBatista <gabrielucasbh2011@gmail.com>
2025-02-02 03:58:26 -0300

    Realizado alterações no layout do CronTask e melhorando algumas funções e criado novos botoes para simplificar cada ação.

---
+++ CronTask/GerenciadorCSV.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CronTask
+{
+    internal class GerenciadorCSV
+    {
+        public static string NomeDaTarefa { get; set; }
+
+        public static DateTime HorarioInicioTarefa { get;  set; }
+
+        public static DateTime HorarioFimTarefa { get;  set; }
+
+        public static TimeSpan TempoGastoTarefa { get; set; }
+
+        public static void SalvarTarefa(string filePath, List<Tarefa> tarefas)
+        {
+            
+            using (StreamWriter sw = new StreamWriter(filePath))
+            {
+                sw.WriteLine("NomeDaTarefa,HorarioInicioTarefa,HorarioFimTarefa,TempoGastoTarefa");
+                foreach (var tarefa in tarefas)
+                {
+                    sw.WriteLine($"{tarefa.NomeDaTarefa},{tarefa.HorarioInicioTarefa:yyyy-MM-dd HH:mm:ss},{tarefa.HorarioFimTarefa:yyyy-MM-dd HH:mm:ss},{tarefa.TempoGastoTarefa:hh\\:mm\\:ss}");
+                }
+            }
+
+        }
+
+        public static List<Tarefa> CarregarCSV(string path)
+        {
+            List<Tarefa> tarefas = new List<Tarefa>();
+
+            if (File.Exists(path))
+            {
+
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    sr.ReadLine();
+                    string linha;
+                    while ((linha = sr.ReadLine()) != null)
+                    {
+                        var dados = linha.Split(',');
+                        tarefas.Add(new Tarefa(NomeDaTarefa = dados[0], HorarioInicioTarefa = DateTime.Parse(dados[1]), HorarioFimTarefa = DateTime.Parse(dados[2]), TempoGastoTarefa = TimeSpan.Parse(dados[3])));
+                    }
+                }
+            }
+            return tarefas;
+        }
+    }
+}
